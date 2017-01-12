12f9ef66e6b306eb8250632b53cd81478e14fcec
xiecy <xiecy_123@hotmail.com>
2017-01-12 16:38:26 +0800

    Initial Commit

---
+++ Now Listening/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Now Listening
+//
+//  Created by Clark on 1/12/17.
+//  Copyright © 2017 Clark Xie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

a4245810427e7a5501442acd8bb5c58afbacb24f
xiecy <xiecy_123@hotmail.com>
2017-01-15 11:26:29 +0800

    completed version that uploads title only

--- Now Listening/API Module.swift
+++ Now Listening/API Module.swift
@@ -15,12 +15,13 @@ class APIModule: NSObject {
     let apiSecret = "3074121c6dd34b7ebf1bb6f72a2cbde0"
     var user: NSString? = "Basic a2lkX0IxR1k1Mk5MZTpjYTU2ZmFmZGNhZDU0NGUzYjk1ZDAxNWI4NTBhYzZlZg=="
     var data: [NSDictionary] = []
+    var fTitle: String = "Loading..."
     
     var urlSession: URLSession?
     var dataTask: URLSessionDataTask?
 
     static let sharedModule = APIModule()
-    
+/*
     func login (completionHandler: @escaping (Data)->Void, errorHandler: @escaping (Error)->Void) {
         let username = "test_user"
         let password = "12345678"
@@ -55,12 +56,22 @@ class APIModule: NSObject {
         }
         
     }
-    
-    func upload (track: String?, completionHandler: @escaping (Data)->Void, errorHandler: @escaping (Error)->Void) {
+*/
+    func upload (track: String?, index: Int, completionHandler: @escaping (Data)->Void, errorHandler: @escaping (Error)->Void) {
+        var self_id: String
+        var f_id: String
+        if (index == 0) {
+            self_id = "5877aa811abe9fc334bb10ef"
+            f_id = "5877a8499bf867084e2b6783"
+        } else {
+            f_id = "5877aa811abe9fc334bb10ef"
+            self_id = "5877a8499bf867084e2b6783"
+        }
+        
         var request = NSMutableURLRequest()
-        let urlStr = "https://baas.kinvey.com/appdata/\(appKey)/tracks/track1"
+        let urlStr = "https://baas.kinvey.com/appdata/\(appKey)/tracks/\(self_id as String)"
         request.url = URL(string: urlStr)
-        request.httpMethod = "POST"
+        request.httpMethod = "PUT"
         
         //let body: String? = track
         //let opt: JSONSerialization.WritingOptions? = .prettyPrinted
@@ -73,7 +84,49 @@ class APIModule: NSObject {
             request.addValue("application/json", forHTTPHeaderField: "Content-Type")
             
             self.urlSession = URLSession.init(configuration: URLSessionConfiguration.default)
-            self.dataTask = self.urlSession!.dataTask(with: request as URLRequest, completionHandler: { (data: Data?, response: URLResponse?, error: Error?) in print ("request completed")
+            self.dataTask = self.urlSession!.dataTask(with: request as URLRequest, completionHandler: { (data: Data?, response: URLResponse?, error: Error?) in print ("upload request completed")
+                //data and response
+                if let failure = error {
+                    errorHandler(failure)
+                } else {
+                    completionHandler(data!)
+                }
+            })
+            self.dataTask?.resume()
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    
+    func retrieve (index: Int, completionHandler: @escaping (Data)->Void, errorHandler: @escaping (Error)->Void) {
+        var self_id: String
+        var f_id: String
+        if (index == 0) {
+            self_id = "5877aa811abe9fc334bb10ef"
+            f_id = "5877a8499bf867084e2b6783"
+        } else {
+            f_id = "5877aa811abe9fc334bb10ef"
+            self_id = "5877a8499bf867084e2b6783"
+        }
+        
+        var request = NSMutableURLRequest()
+        let urlStr = "https://baas.kinvey.com/appdata/\(appKey)/tracks/\(f_id as String)"
+        print(urlStr)
+        request.url = URL(string: urlStr)
+        request.httpMethod = "GET"
+        
+        //let body: String? = track
+        //let opt: JSONSerialization.WritingOptions? = .prettyPrinted
+        //let jsonBody = ["title": track]
+        
+        do {
+            //let bodyData = try JSONSerialization.data(withJSONObject: jsonBody as Any)
+            //request.httpBody = bodyData
+            request.addValue(self.user as! String, forHTTPHeaderField: "Authorization")
+            //request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+            
+            self.urlSession = URLSession.init(configuration: URLSessionConfiguration.default)
+            self.dataTask = self.urlSession!.dataTask(with: request as URLRequest, completionHandler: { (data: Data?, response: URLResponse?, error: Error?) in print ("retrieve request completed")
                 //data and response
                 if let failure = error {
                     errorHandler(failure)
--- Now Listening/ViewController.swift
+++ Now Listening/ViewController.swift
@@ -15,7 +15,14 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        getPlayerStatus()
+        initDisplay()
+        //updateDisplay()
+        /*
+        for _ in 1...30 {
+            _ = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(self.updateDisplay), userInfo: nil, repeats: false)
+        }
+        */
+        var timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(self.updateDisplay), userInfo: nil, repeats: true)
     }
 
     override func didReceiveMemoryWarning() {
@@ -27,26 +34,46 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var songTitle: UILabel!
     @IBOutlet weak var songArtist: UILabel!
+    @IBOutlet weak var fSongTitle: UILabel!
+    @IBOutlet weak var YListening: UILabel!
+    @IBOutlet weak var FListening: UILabel!
+    @IBOutlet weak var userSwitchLabel: UILabel!
+    @IBOutlet weak var userSwitch: UISegmentedControl!
     
-    func getPlayerStatus() {
+    func initDisplay() {
+        YListening.text = "You're listening to"
+        FListening.text = "Your friend is listening to"
+        
+        songTitle.text = "Loading..."
+        songArtist.text = "Loading..."
+        fSongTitle.text = "Loading..."
+        
+        //userSwitch configuration
+        userSwitchLabel.text = "You are"
+        userSwitch.setTitle("User1", forSegmentAt: 0)
+        userSwitch.setTitle("User2", forSegmentAt: 1)
+    }
+    
+    func updateDisplay() {
+        var self_index: Int = userSwitch.selectedSegmentIndex
+        
         let player = MPMusicPlayerController.systemMusicPlayer()
         if let mediaItem = player.nowPlayingItem {
             let title: String = mediaItem.value(forProperty: MPMediaItemPropertyTitle) as! String
             let albumTitle: String = mediaItem.value(forProperty: MPMediaItemPropertyAlbumTitle) as! String
             let artist: String = mediaItem.value(forProperty: MPMediaItemPropertyArtist) as! String
             
-            songTitle.text = title;
-            songArtist.text = artist;
-            
+            songTitle.text = title
+            songArtist.text = artist
             print("\(title) on \(albumTitle) by \(artist)")
-            
-            makeUploadRequest(track: title)
+            makeUploadRequest(track: title, index: self_index)
+            self.fSongTitle.text = makeRetrieveRequest(index: self_index)
         }
         else {
             print("error")
         }
     }
-    
+/*
     func makeLoginRequest() {
         APIModule.sharedModule.login(completionHandler: { data in
             do {
@@ -58,19 +85,39 @@ class ViewController: UIViewController {
             } catch {
                 print(error.localizedDescription)
             }
-            
         }) { error in
             print("error:\(error)")
         }
     }
+*/
     
-    func makeUploadRequest (track: String?) {
-        APIModule.sharedModule.upload(track: track, completionHandler: { data in
+    func makeUploadRequest (track: String?, index: Int) {
+        APIModule.sharedModule.upload(track: track, index: index, completionHandler: { data in
             print("upload success")
         }, errorHandler: { error in
             print("error:\(error)")
         })
     }
-
+    
+    func makeRetrieveRequest(index: Int) -> String {
+        var response: String?
+        APIModule.sharedModule.retrieve(index: index, completionHandler: { data in
+            do {
+                let responseDict = try JSONSerialization.jsonObject(with: data) as! NSDictionary
+                print(responseDict)
+                let responseTitle = responseDict["title"] as! NSString
+                print("response title is \(responseTitle)")
+                APIModule.sharedModule.fTitle = responseTitle as String
+                print(APIModule.sharedModule.fTitle)
+            } catch {
+                print(error.localizedDescription)
+            }
+        }) { error in
+            print("error:\(error)")
+        }
+        let fTitle: String = APIModule.sharedModule.fTitle as String
+        print("fTitle = \(fTitle)")
+        return fTitle
+    }
 }
 
